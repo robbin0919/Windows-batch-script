885a340e86cba6ffd8d1c297e778a56cf4e503c8
robbin lee <66932401+robbin0919@users.noreply.github.com>
2024-12-29 15:04:11 +0800

    Update WindowsBatchScript-to-parse-CSV-file.bat

--- WindowsBatchScript-to-parse-CSV-file.bat
+++ WindowsBatchScript-to-parse-CSV-file.bat
@@ -1,1 +1,35 @@
+rem 
+@echo off
+setlocal enabledelayedexpansion
+set work_disk=D:
+set work_dir=F:\LAB
+set checkmarx=F:\LAB\checkmarx_report.csv
+set "delimiter=,"
+set "delimiter2=^"
+set var1=""
+set "var2=^^" 
+set "var3=^ ^" 
+set var4=""
+set "keyword2="" 
+set "replacement2=" 
+set "keyword1=","" 
+set "replacement1=^" 
+set "keyword=^^" 
+set "replacement=^ ^" 
+!work_disk!
+cd !work_dir!
+
+ 
+ 
+ 
+for /F "tokens=* delims=" %%a in ('powershell "import-csv %checkmarx%  |convertto-csv "') do (
+set var1=%%a
+echo source:[!var1!] 
+set "var4=!var1:%keyword1%=%replacement1%!" 
+set "var4=!var4:%keyword%=%replacement%!" 
+set "var4=!var4:%keyword%=%replacement%!" 
+set "var4=!var4:%keyword2%=%replacement2%!" 
+echo b:[!var4!]  
+
+）
 
